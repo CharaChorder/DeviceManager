c2e3850082fe29dd6f1944f8ea370bfb1922f8ef
Thea Schöbl <dev@theaninova.de>
2023-12-21 20:47:06 +0100

    fix: maybe fix cloudflare pages loop

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,5 +2,5 @@ import {redirect} from "@sveltejs/kit"
 import type {PageLoad} from "./$types"
 
 export const load = (() => {
-  throw redirect(302, "/config")
+  throw redirect(302, "/config/")
 }) satisfies PageLoad
