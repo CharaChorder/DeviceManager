fb1f5b7ec7ad4ec5b5fb99179fb04f0298aa99a8
Thea Schöbl <dev@theaninova.de>
2025-02-14 14:55:06 +0100

    fix: can't type in chords

--- src/routes/(app)/config/chords/input-converter.ts
+++ src/routes/(app)/config/chords/input-converter.ts
@@ -1,12 +1,16 @@
 import { KEYMAP_IDS, KEYMAP_KEYCODES } from "$lib/serial/keymap-codes";
+import { get } from "svelte/store";
 
 export function inputToAction(
   event: KeyboardEvent,
   useKeycodes?: boolean,
 ): number | undefined {
   if (useKeycodes) {
-    return KEYMAP_KEYCODES.get(event.code);
+    return get(KEYMAP_KEYCODES).get(event.code);
   } else {
-    return KEYMAP_IDS.get(event.key)?.code ?? KEYMAP_KEYCODES.get(event.code);
+    return (
+      get(KEYMAP_IDS).get(event.key)?.code ??
+      get(KEYMAP_KEYCODES).get(event.code)
+    );
   }
 }
