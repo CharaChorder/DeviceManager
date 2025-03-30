9264a2c96cb07f2e349de714e55fd45b349b0384
izeren <izeren.ml@gmail.com>
2025-03-30 21:06:00 +0100

    Use modern compiler for css processing in vite (to remove SASS 2.0.0 warnings on deprecated JS API usage)

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,13 @@ export default defineConfig({
   define: {
     global: "window",
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   envPrefix: ["TAURI_", "VITE_"],
   plugins: [
     ViteYaml(),
